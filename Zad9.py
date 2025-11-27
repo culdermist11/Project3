47a14cb5eb2a1183a80cafd952ae45ca3233bd0d
Ivan <kovalenkoivol9@gmail.com>
2025-11-27 21:10:59 +0100

    Zad 9

---
+++ Zad9.py
@@ -0,0 +1,27 @@
+# Zadanie 9
+
+tekst = str(input("Podaj imię"))
+print(f"Witaj {tekst}")
+
+tekst = str(input("Podaj wiek"))
+print(f"Twój wiek to: {tekst}")
+
+firstname = str(input("Podaj imię"))
+lastname = str(input("Podaj nazwisko"))
+firstname = firstname[0]
+lastname = lastname[0]
+(print(f"{firstname} {lastname}"))
+
+tekst = str(input("Podaj tekst"))
+for x in range(5):
+    print(tekst)
+
+tekst1 = str(input("Podaj tekst 1"))
+tekst2 = str(input("Podaj tekst 2"))
+print(f"{tekst1}{tekst2}")
+
+a = input("Podaj pierwszy łańcuch: ")
+b = input("Podaj drugi łańcuch: ")
+c = a
+
+print("Pierwsza połowa pierwszego łańcucha:", c)
