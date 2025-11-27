917d134b343700c9f353e821707ed19c7d4b1829
Ivan <kovalenkoivol9@gmail.com>
2025-11-27 19:48:03 +0100

    Zad 3

--- Zad3.py
+++ Zad3.py
@@ -2,16 +2,7 @@
 
 ulice = ["Jegodowa", "Lipowa", "Kwiatowa", "Kasztanova", "Polna"]
 
-def generuj_adresy(ulice):
-    adresy = []
-    for ulica in ulice:
-        for blok in range(1, 6):
-            for lokal in range(1, 11):
-                adres = f"{ulica} {blok}/{lokal}"
-                adresy.append(adres)
-    return adresy
-
-adresy = generuj_adresy(ulice)
-
-for adres in adresy:
-    print(adres)
+for ulica in ulice:
+    for blok in range(1, 6):
+        for lokal in range(1, 11):
+            print(f"{ulica} {blok}/{lokal}")
