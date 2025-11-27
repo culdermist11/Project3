ef9d21211b95c4bfee0510a22bd1417c4f5ff5fd
Ivan <kovalenkoivol9@gmail.com>
2025-11-27 19:36:52 +0100

    Zad 6

---
+++ Zad6.py
@@ -0,0 +1,25 @@
+# Zadanie 6
+
+zmienna = int(input("Podaj liczbe całkowitą: "))
+
+while zmienna > 0:
+    zmienna = int(input("Podaj liczbe całkowitą: "))
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# integer 1
+# float 1.0
+# string 'sadasd'
+# bool True False
